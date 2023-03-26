2397c4ea9f0df4d33d576c9729e0c150430a11c8
JiamingKe <jiamingke@outlook.com>
2023-03-26 20:13:07 +0100

    implement alligator indicator

---
+++ indicator_alligator_test.go
@@ -0,0 +1,26 @@
+package techan
+
+import "testing"
+
+func TestAlligatorIndicator(t *testing.T) {
+	const (
+		window = 5
+		offset = 3
+	)
+	t.Run("initial value", func(t *testing.T) {
+
+		series := generateTestTimeSeries()
+		indicator := NewAlligatorIndicator(series, window, offset)
+
+		decimalEquals(t, 28139.2400, indicator.Calculate(window+offset-1))
+	})
+
+	t.Run("second value", func(t *testing.T) {
+
+		series := generateTestTimeSeries()
+		indicator := NewAlligatorIndicator(series, window, offset)
+
+		decimalEquals(t, 28145.5320, indicator.Calculate(window+offset))
+	})
+
+}
